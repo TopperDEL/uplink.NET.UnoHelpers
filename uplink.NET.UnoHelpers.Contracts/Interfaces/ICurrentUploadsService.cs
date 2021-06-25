b1923552ab6e150cc22f53c4f39b6c45da9f72a6
TopperDEL <github_com@tparth.de>
2021-06-25 14:02:04 +0200

    feat: current state

---
+++ uplink.NET.UnoHelpers.Contracts/Interfaces/ICurrentUploadsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.UnoHelpers.Contracts.Interfaces
+{
+    public interface ICurrentUploadsService
+    {
+        bool HasCurrentUploads { get; }
+    }
+}
---
+++ uplink.NET.UnoHelpers.Contracts/Models/CurrentUpload.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uplink.NET.UnoHelpers.Contracts.Models
+{
+    public class CurrentUpload
+    {
+        public string Description { get; set; }
+        public int TotalBytes { get; set; }
+        public int BytesCompleted { get; set; }
+        public float PercentCompleted { get; set; }
+    }
+}
