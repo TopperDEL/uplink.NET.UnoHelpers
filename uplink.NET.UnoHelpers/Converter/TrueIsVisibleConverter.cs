b1c71b23a4aa669f5e8e4f71ec64451e3de6be40
TopperDEL <github_com@tparth.de>
2021-06-28 11:00:15 +0200

    feat: CurrentUpload-Entries are visible

---
+++ uplink.NET.UnoHelpers/Converter/TrueIsVisibleConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace uplink.NET.UnoHelpers.Converter
+{
+    public class TrueIsVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                bool currentValue = (bool)value;
+                if (currentValue)
+                    return Visibility.Visible;
+                else
+                    return Visibility.Collapsed;
+            }
+            catch
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
