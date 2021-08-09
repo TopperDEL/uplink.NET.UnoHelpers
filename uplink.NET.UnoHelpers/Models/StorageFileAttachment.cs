854581a86b791d7bd011c3f84816e32a993767f0
TopperDEL <github_com@tparth.de>
2021-08-09 21:56:54 +0200

    feat: StorageFileAttachment

---
+++ uplink.NET.UnoHelpers/Models/StorageFileAttachment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using uplink.NET.UnoHelpers.Contracts.Models;
+using Windows.Storage;
+
+namespace uplink.NET.UnoHelpers.Models
+{
+    public class StorageFileAttachment : Attachment
+    {
+        public StorageFile StorageFile { get; set; }
+
+        public new Stream AttachmentData
+        {
+            get
+            {
+                return StorageFile?.OpenReadAsync().GetResults().AsStream();
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
